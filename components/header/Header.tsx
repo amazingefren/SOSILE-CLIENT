a539b9e2bb29545d5440a0b087451f18621170b2
amazingefren <efrentheroker@hotmail.com>
2021-08-23 18:03:55 -0600

    header + search elements, no functionality

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,10 +1,16 @@
-import HeaderStyle from "./header.module.scss"
+import HeaderStyle from "./header.module.scss";
 export default function () {
   return (
     <header id={HeaderStyle.container}>
       <div id={HeaderStyle.sosile}>
-        <div id={HeaderStyle.logo}/>
+        <div id={HeaderStyle.logo} />
         <div id={HeaderStyle.name}>SOSILE</div>
+      </div>
+      {/*@TODO GLOBAL STATE MANAGEMENT*/}
+      <div id={HeaderStyle.search}>
+          <div id={HeaderStyle.searchBar}>
+            <input id={HeaderStyle.searchBarInput} placeholder="search"/>
+        </div>
       </div>
     </header>
   );
